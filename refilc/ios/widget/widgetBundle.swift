cc52c58279f3ada5d5254f588d77c41018633da6
Marton Kiss <kima.public@gmail.com>
2025-02-12 23:28:40 +0100

    started ios home screen widget

---
+++ refilc/ios/widget/widgetBundle.swift
@@ -0,0 +1,17 @@
+//
+//  widgetBundle.swift
+//  widget
+//
+//  Created by Márton Kiss on 2025. 02. 12..
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct widgetBundle: WidgetBundle {
+    var body: some Widget {
+        widget()
+        widgetControl()
+    }
+}
