f78617bfa16678d318ef4e63044c857bb222775f
Pearoo <63553775+rnxm@users.noreply.github.com>
2025-02-23 14:26:33 +0100

    Optimized app
    By deleting it. Honestly, there's literally no need to keep the current code base. Quote from kima: 'the old developer's code is unmaintainable'. So why don't you just rewrite the app from scratch then? Coward

--- refilc/android/app/src/main/java/hu/refilc/naplo/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package hu.refilc.naplo
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- refilc_kreta_api/lib/controllers/profile_controller.dart
+++
@@ -1,5 +0,0 @@
-import 'package:flutter/foundation.dart';
-
-class ProfileController extends ChangeNotifier {
-  //todo: profile controller (pw change, etc)
-}
